13b50f03f8fbc2eca13772c57c5b04a9dcf8013a
Joseph Yang <before30@gmail.com>
2020-03-07 15:22:10 +0900

    Add banking app

---
+++ banking-app/src/main/java/cc/before30/homeex/banking/BankingApplication.java
@@ -0,0 +1,20 @@
+package cc.before30.homeex.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * BankingApplication
+ *
+ * @author before30
+ * @since 2020/03/07
+ */
+
+@SpringBootApplication
+public class BankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApplication.class, args);
+    }
+
+}
